5532ca0c8bac32f7fc0908e5178e1c88f231f6a6
sofia.viarani <sofia.viarani.stud@ispascalcomandini.it>
2021-11-19 08:18:39 +0100

    LIbro:aggiunta attributi e proprieta

---
+++ verifica/Libro.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace verifica
+{
+    class Libro
+    {
+        private string _autore;
+        private string _titolo;
+        private DateTime _annoDiPublicazione;
+        private string _editore;
+        private int _numeroPagine;
+        public string Autore
+        {
+            get
+            {
+                return _autore;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new Exception("errore nell'inserimento");
+                }
+                _autore = value;
+            }
+        }
+        public string Titolo
+        {
+            get
+            {
+                return _titolo;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new Exception("errore nell'inserimento");
+                }
+                _titolo = value;
+            }
+        }
+        public string Editore
+        {
+            get
+            {
+                return _editore;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new Exception("errore nell'inserimento");
+                }
+                _editore = value;
+            }
+        }
+        public DateTime AnnoDiPublicazione
+        {
+            get
+            {
+                return _annoDiPublicazione;
+            }
+            set
+            {
+  
+                _annoDiPublicazione = value;
+            }
+        }
+        public int NumeroPagine
+        {
+            get
+            {
+                return _numeroPagine;
+            }
+            set
+            {
+                if (value<0)
+                {
+                    throw new Exception("errore nell'inserimento");
+                }
+                _numeroPagine = value;
+            }
+        }
+
+    }
+}
